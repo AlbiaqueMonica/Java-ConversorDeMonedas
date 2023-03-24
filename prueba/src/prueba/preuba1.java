2eafbc9cfa81a7d12d67972bed730c75bf5d91ff
AlbiaqueMonica <monic.albiaque@gmail.com>
2023-03-24 04:33:14 -0300

    add

--- prueba/src/prueba/preuba1.java
+++ prueba/src/prueba/preuba1.java
@@ -5,7 +5,7 @@ public class preuba1 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
-		System.out.println("es una prueba");
+		System.out.println("es una gjgf prueba");
 
 	}
 
